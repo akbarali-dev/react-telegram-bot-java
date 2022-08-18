30a7a250039cb01c707839779551c0004af4e5d2
Akbarali Asqaraliyev <akbaraliasqaraliyev0610@gmail.com>
2022-08-18 14:40:52 +0500

    dsdferkdfhg

--- src/main/java/uz/akbarali/foodbotwithreact/controller/ProductController.java
+++ src/main/java/uz/akbarali/foodbotwithreact/controller/ProductController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import uz.akbarali.foodbotwithreact.payload.ApiResponse;
 import uz.akbarali.foodbotwithreact.repository.ProductRepository;
 import uz.akbarali.foodbotwithreact.service.ProductService;
-@CrossOrigin(origins = "http://localhost:3000")
+//@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/api/v1/product")
 public class ProductController {
